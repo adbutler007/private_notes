c7fec338c440e7b625ec69929e9c12aca9866a67
Adam Butler <adbutler007@gmail.com>
2025-11-19 10:56:33 -0500

    chore: Update SHA256 for notarized v0.2.2

--- audio_summary_app/homebrew-cask/audio-summary.rb
+++ audio_summary_app/homebrew-cask/audio-summary.rb
@@ -1,6 +1,6 @@
 cask "audio-summary" do
   version "0.2.2"
-  sha256 "f5039d49868d926cf2daf44675c4eb5f526b730cb144f86f4c2bfc3ea1d65a68"  # Update after building: shasum -a 256 AudioSummary-#{version}.zip
+  sha256 "1c46ae91123d71c82c9b55e321a69feecef97a9b1180dcdb9399bf74ec8e0f2c"  # Update after building: shasum -a 256 AudioSummary-#{version}.zip
 
   url "https://github.com/adbutler007/private_notes/releases/download/v#{version}/AudioSummary-#{version}.zip"
   name "Audio Summary"
